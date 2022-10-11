c93f3693304d9e400fa8d5aab8c3fd4c1c8b4d22
zm21262odanaka <104361960+zm21262odanaka@users.noreply.github.com>
2022-10-11 10:36:26 +0900

    Update ex6-5.js

--- ex6-5.js
+++ ex6-5.js
@@ -4,14 +4,16 @@ function sayhello65(){
 }
 function taro65(){
  who65 ='太郎';
+}
 
-function taro65()
+function taro65(){
  alert('太郎が来た');
 }
  
 function hanako65(){
  who65 '花子';
-
-function hanako65()
+{
+ 
+function hanako65(){
  alert('花子が来た');
 }

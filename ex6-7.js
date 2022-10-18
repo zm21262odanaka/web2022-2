e4050da7cc968110ba925807bb9bcd679e2b09db
zm21262odanaka <104361960+zm21262odanaka@users.noreply.github.com>
2022-10-18 10:30:39 +0900

    Update ex6-7.js

--- ex6-7.js
+++ ex6-7.js
@@ -1,11 +1,6 @@
 var who67 = 'world';
-function sayhello67(){
-  alert('Hello,'+who67+'!');
+function sayhello67() {
+  alert('Hello, ' + who67 + '!');
 }
-
-function someone(x){
-  who67=x;
-}
-
-  
-  
+function someone(x) {
+  who67 = x;}

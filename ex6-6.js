2c8fa24234cfb40eaa09985ff8268dc867c7886c
zm21262odanaka <104361960+zm21262odanaka@users.noreply.github.com>
2022-10-18 09:56:24 +0900

    Update ex6-6.js

--- ex6-6.js
+++ ex6-6.js
@@ -2,11 +2,19 @@ var who66 ='world';
 function sayhello66(){
  alert('Hello,'+who66+'!');
 }
-function taro66(){who66='太郎'; alert(who66+'が来た!')
+
+function taro66(){who66='太郎';
+}
+
+function taro66(){who66='太郎';
+alert(who66+'が来た'+'!');
 }
  
-function hanako66(){who66='花子'; alert(who66+'が来た！')
+function hanako66(){who66='花子';
 }
- 
+
+function hanako66(){who66='花子';
+alert(who66+'が来た'+'!');
+}
 
  

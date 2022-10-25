3cbaea42c7d63f53807912f998753028cdc91932
zm21262odanaka <104361960+zm21262odanaka@users.noreply.github.com>
2022-10-25 10:05:00 +0900

    Create ex6-8.js

---
+++ ex6-8.js
@@ -0,0 +1,9 @@
+function sayhello68(){
+  alert('Hello, ' + document.getElementById('who68').innerHTML + '!');
+}
+function taro68(){
+  document.getElementById('who68').innerHTML = '太郎';
+}
+function hanako68(){
+  dicument.getElementById('who68').innerHTML = '花子';
+}

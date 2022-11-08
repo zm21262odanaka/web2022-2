200cf32a0909118dc0ff810a8abba7c7acfa4044
zm21262odanaka <104361960+zm21262odanaka@users.noreply.github.com>
2022-11-08 09:21:27 +0900

    Update ex6-88.js

--- ex6-88.js
+++ ex6-88.js
@@ -1,4 +1,4 @@
 function imgchange1()
 {
-document.getElementById('img1').innerHTML = '<img src="./image/abm00047230.jpg">';
+document.getElementById('img1').innerHTML = '<img src="./image/道.jpg">';
 }

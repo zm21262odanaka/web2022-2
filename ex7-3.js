b798a517459d1e0d917bb11085ac81b9ae33f8e4
zm21262odanaka <104361960+zm21262odanaka@users.noreply.github.com>
2022-12-15 00:53:44 +0900

    Create ex7-3.js

---
+++ ex7-3.js
@@ -0,0 +1,6 @@
+function add(){
+ var x = document.getElementById('inout1').value;
+ var y = document.getElementById('input2').value;
+ var z = document.getElementById('input3').value;
+ alert(x/(y*z));
+}

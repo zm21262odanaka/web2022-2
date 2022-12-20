3a5de78a00ab69621cd463eaacbc63264fdec4dc
zm21262odanaka <104361960+zm21262odanaka@users.noreply.github.com>
2022-12-20 09:43:04 +0900

    Update ex7-3.js

--- ex7-3.js
+++ ex7-3.js
@@ -2,5 +2,5 @@ function add(){
  var x = document.getElementById('inout1').value;
  var y = document.getElementById('input2').value;
  var z = document.getElementById('input3').value;
- alert(x/(y*z));
+ alert(Number(x)/(Number(y)*Number(z)));
 }

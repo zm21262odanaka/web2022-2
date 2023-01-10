ceddbcac1b25422fedfeabae5771a670e8f0fbec
zm21262odanaka <104361960+zm21262odanaka@users.noreply.github.com>
2023-01-10 11:43:25 +0900

    Update ex7-2.js

--- ex7-2.js
+++ ex7-2.js
@@ -1,5 +1,5 @@
-
-var x = document.getElementById('input1').value;
-var y = document.getElementById('input2').value;
-alert(Number(x)+Number(y));
-
+function add() {
+ var x =(document.getElementById('input1').value);
+ var y =(document.getElementById('input2').value);
+ alert (Number(x)+Number(y));
+}

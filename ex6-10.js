1bbd526a3f975b23885020d4761e98064dfe7d29
zm21262odanaka <104361960+zm21262odanaka@users.noreply.github.com>
2023-01-10 12:53:48 +0900

    Update ex6-10.js

--- ex6-10.js
+++ ex6-10.js
@@ -1,9 +1,2 @@
-function imgchange2() 
-{
-document.getElementById('logo').src='./image/bulboff.gif';
-}
-
-function imgchange3()
-{
-document.getElementById('logo').src='./image/bulbon.gif.';
-}
+function imgchange2(){document.getElementById('logo').src='./image/bulbon.gif';}
+function imgchange3(){document.getElementById('logo').src='./image/logo.png';}

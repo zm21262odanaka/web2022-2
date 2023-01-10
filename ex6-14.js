b9b1aa0eeff0c930e068853b9a938ecb1969ac18
zm21262odanaka <104361960+zm21262odanaka@users.noreply.github.com>
2023-01-10 12:18:52 +0900

    Update ex6-14.js

--- ex6-14.js
+++ ex6-14.js
@@ -1,2 +1,2 @@
-function imgchange5() {document.getElementById('logo1').src ="./image/城西大学ロゴ.png";}
+function imgchange5() {document.getElementById('logo1').src ="./image/miti.jpg";}
 function imgchange6() {document.getElementById('logo1').src ="./image/logo.png";}

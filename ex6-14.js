1ac03e6c428fb9b794f8fe4a349f975f8fa2c649
zm21262odanaka <104361960+zm21262odanaka@users.noreply.github.com>
2023-01-10 12:43:25 +0900

    Update ex6-14.js

--- ex6-14.js
+++ ex6-14.js
@@ -1,3 +1,3 @@
-function imgchange5() {document.getElementById('logo1').src ="./image/bulbon.gif";}
+function imgchange5() {document.getElementById('logo1').src ="./image/miti.jpg";}
 function imgchange6() {document.getElementById('logo1').src ="./image/logo.png";}
 

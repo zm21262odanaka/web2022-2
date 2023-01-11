b93a722c8839112fd81987455ee26f8e120004f1
zm21262odanaka <104361960+zm21262odanaka@users.noreply.github.com>
2023-01-11 22:17:31 +0900

    Update ex7-7.js

--- ex7-7.js
+++ ex7-7.js
@@ -1,7 +1,8 @@
-function nazo77(){
- if(document.getElementById('answer７７').value='夢'){
-   alert('あたり!');
- }else{
-   alert('はずれ！');
+function nazo77() {
+if (document.getElementById('answer77').value = '夢') {
+ alert('あたり！');
+} 
+else {
+ alert('はずれ！');
  }
 }

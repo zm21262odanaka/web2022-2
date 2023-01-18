7e357846fc46b7f559ee851231ceab616ca9d260
zm21262odanaka <104361960+zm21262odanaka@users.noreply.github.com>
2023-01-18 17:38:22 +0900

    Create ex7-10.js

---
+++ ex7-10.js
@@ -0,0 +1,18 @@
+function add710(){
+var x =document.getElementById('input5').value;
+var y =document.getElementById('input6').value;
+var z =(Number(y)/Number(x*x)*10000);
+if(z <= 18.5){
+alert(Number(y)/Number(x*x)*10000);
+ alert('低体重');
+}
+else if(z < 25){
+alert(Number(y)/Number(x*x)*10000);
+ alert('普通');
+}
+else 
+alert(Number(y)/Number(x*x)*10000);
+ alert('肥満');
+}
+
+

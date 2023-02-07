48a0085673b3827807ea229aa86e8b37efbe6395
zm21262odanaka <104361960+zm21262odanaka@users.noreply.github.com>
2023-02-08 01:37:29 +0900

    Update ex7-10.js

--- ex7-10.js
+++ ex7-10.js
@@ -1,18 +1,17 @@
-function add710(){
+function add3(){
 var x =document.getElementById('input5').value;
 var y =document.getElementById('input6').value;
-var z =(Number(y)/Number(x*x)*10000);
-if(z <= 18.5){
-alert(Number(y)/Number(x*x)*10000);
+var z=Number(y)/Number(x*x)*10000;
+if(z <=18.5){
+ alert(Number(y)/Number(x*x)*10000);
  alert('低体重');
 }
-else if(z < 25){
-alert(Number(y)/Number(x*x)*10000);
+else if(z <25){
+ alert(Number(y)/Number(x*x)*10000);
  alert('普通');
 }
-else 
-alert(Number(y)/Number(x*x)*10000);
+else{
+ alert(Number(y)/Number(x*x)*10000);
  alert('肥満');
+ }
 }
-
-

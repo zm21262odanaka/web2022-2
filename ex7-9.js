71df65efc101756a04e602f4e98a801e14917168
zm21262odanaka <104361960+zm21262odanaka@users.noreply.github.com>
2023-02-08 10:15:01 +0900

    Create ex7-9.js

---
+++ ex7-9.js
@@ -0,0 +1,11 @@
+function nazo5(){
+if(document.getElementById('answer5').value=='夢'){
+ alert('あたり！');
+}
+else if(document.getElementById('answer5').value=='ゆめ'){
+ alert('あたり！');
+}
+else{
+ alert('はずれ!');
+ }
+}
